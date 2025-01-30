e8ae138684862f0d9192a89a96f83ac36b3e8400
stjoo0925 <stjoo0925@gmail.com>
2025-01-30 14:07:01 +0000

    chore : methods 연습

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
- 
+# ---------------------------------
+# methods 파트
+
+# 파이선의 데이터 구조는 list, tuple, dictionary
+# data structure(자료 구조)는 데이터를 구조화 하고 싶을 때 사용한다
+
+mon = "Mon"
+tue = "Tue"
+wed = "Wed"
+thu = "Thu"
+fri = "Fri"
