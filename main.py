16a3268a6a53396bbe43e4d7c25966192ac555d3
Stjoo0925 <stjoo0925@gmail.com>
2025-02-01 23:48:43 +0900

    chore : methods 연습

--- main.py
+++ main.py
@@ -4,8 +4,21 @@
 # 파이선의 데이터 구조는 list, tuple, dictionary
 # data structure(자료 구조)는 데이터를 구조화 하고 싶을 때 사용한다
 
-mon = "Mon"
-tue = "Tue"
-wed = "Wed"
-thu = "Thu"
-fri = "Fri"
+# mon = "Mon"
+# tue = "Tue"
+# wed = "Wed"
+# thu = "Thu"
+# fri = "Fri"
+
+# list(배열)
+# days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+
+# print(days_of_week)
+
+name = "soontae"
+
+# upper()같은 변수와 연결되어 있는 함수들이 있다 이때 이런 함수들을 methods라고 한다
+# upper 는 문자열을 대문자로 바꿔주는 메서드 이다
+print(name.upper())
+print(name.startswith("s"))
+print(name.replace("o", "!"))
