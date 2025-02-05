78d0dcc240801e0a996ab6d5e9b9bc02c37ad9e7
Stjoo0925 <stjoo0925@gmail.com>
2025-02-05 11:58:01 +0900

    chore : dicts 연습

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri"]
 # lists 파트
 
 # 특정 데이터 개수 세기
-print(days_of_week.count("Wed"))
+# print(days_of_week.count("Wed"))
 
 # 데이터 추가하기
 days_of_week.append("Sat")
@@ -37,17 +37,49 @@ days_of_week.append("Sun")
 # 특정 데이터 삭제하기
 days_of_week.remove("Fri")
 
-print(days_of_week)
+# print(days_of_week)
 
 # 특정 인덱스의 값 가져오기
 
-print(days_of_week[3])
+# print(days_of_week[3])
 
 # ---------------------------------
 # tuples 파트
 
 # tuple은 [] 대신 ()를 사용한다
-# tuple은 변경이 불가능하다
+# tuple은 변경이 불가능하다 | 불변성
+
 day = ("Mon", "Tue", "Wed", "Thu", "Fri")
 
-print(day.count("Mon"))
+# print(day.count("Mon"))
+
+# ---------------------------------
+# Dicts 파트
+
+player = {
+    'name' : 'soontae',
+    'age' : 20,
+    'alive' : True,
+    'fav_food' : ['pizza', 'burger']
+}
+# 딕셔너리는 많은 속성(타입)들을 가지고 있는 데이터를 만들 때 사용된다
+
+# print(player.get('age'))
+
+# print(player.get('fav_food'))
+# print(player['fav_food']) | 위와 동일하게 작동함
+
+# 딕셔너리에 데이터 삭제하기
+# print(player)
+# print(player.pop('age'))
+# print(player)
+
+
+# 딕셔너리에 데이터 추가하기
+# print(player)
+# player['xp'] = 1500
+# print(player)
+
+# 딕셔너리 리스트에 데이터를 추가하는 방법
+player['fav_food'].append("noodle")
+print(player.get('fav_food'))
